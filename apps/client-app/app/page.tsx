7f6caf22c171c3cb7e4bb8bc10eb09fe8cfed5d6
unique3900 <parasharneupane3900@gmail.com>
2024-05-30 11:46:12 +0545

    Login with google and github added

--- apps/client-app/app/page.tsx
+++ apps/client-app/app/page.tsx
@@ -1,12 +1,19 @@
 
 import { store } from '@repo/store/store'
+import { getServerSession } from 'next-auth'
 import React from 'react'
+import { authOptions } from '../lib/auth';
+const getUser=async()=>{
+  const session= await getServerSession(authOptions);
+  console.log(session)
+  return session
+}
 
 
-const page = () => {
-
+const page =async () => {
+  const session=await getUser();
   return (
-    <div className=''>hey</div>
+    <div className=''>hey {session?.user.email}</div>
   )
 }
 
